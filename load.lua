b79407ce10b28f05cd575334de68b0bd6877aa44
print("\226\153\165") <86081255+GoodBoy08@users.noreply.github.com>
2021-10-10 04:30:08 -0700

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 5041144419 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/GoodBoy08/sob/main/games/scp-roleplay.lua"))()
-elseif game.PlaceId = 4991214437 then
+elseif game.PlaceId == 4991214437 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/GoodBoy08/sob/main/games/town.lua"))()
 end
